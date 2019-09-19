e81245b7ef80378f67d35045da53f32c1bf90f80
Anivedh Auradkar <42490394+anivedh555@users.noreply.github.com>
2019-09-20 00:09:25 +0530

    Add files via upload

---
+++ vmd.cpp
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(void)
+{
+ 
+   while(true){  
+        long c;
+	    scanf("%c",&c);     
+            if(c=='#') return 0;   
+                            c=tolower(c);    
+            if(c=='e')printf("q");
+
+if(c=='r')printf("w");
+if(c=='t')printf("e");
+if(c=='y')printf("r");
+if(c=='u')printf("t");
+if(c=='i')printf("y");
+if(c=='o')printf("u");
+if(c=='p')printf("i");
+if(c=='[')printf("o");
+if(c==']')printf("p");
+if(c=='\\')printf("[");
+if(c=='d')printf("a");
+if(c=='f')printf("s");
+if(c=='g')printf("d");
+if(c=='h')printf("f");
+if(c=='j')printf("g");
+if(c=='k')printf("h");
+if(c=='l')printf("j");
+if(c==';')printf("k");
+if(c=='\'')printf("l");
+if(c=='c')printf("z");
+if(c=='v')printf("x");
+if(c=='b')printf("c");
+if(c=='n')printf("v");
+if(c=='m')printf("b");
+if(c==',')printf("n");
+if(c=='.')printf("m");
+if(c=='/')printf(",");
+if(c=='2')printf("`");
+if(c=='3')printf("1");
+if(c=='4')printf("2");
+if(c=='5')printf("3");
+if(c=='6')printf("4");
+if(c=='7')printf("5");
+if(c=='8')printf("6");
+if(c=='9')printf("7");
+if(c=='0')printf("8");
+if(c=='-')printf("9");
+if(c=='=')printf("0");
+if(c=='#')printf("1");
+if(c=='$')printf("2");
+if(c=='%')printf("3");
+if(c=='^')printf("4");
+if(c=='&')printf("5");
+if(c=='*')printf("6");
+if(c=='(')printf("7");
+if(c==')')printf("8");
+if(c=='_')printf("9");
+if(c=='+')printf("0");
+if(c=='{')printf("o");
+if(c=='}')printf("p");
+if(c=='|')printf("[");
+if(c==':')printf("k");
+if(c=='"')printf("l");
+if(c=='<')printf("n");
+if(c=='>')printf("m");
+if(c=='?')printf(",");             
+
+   }  
+  return 0;
+}
